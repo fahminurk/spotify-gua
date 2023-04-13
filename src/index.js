eeaacaa4e9b47b8750b43dd8fd4b224885340731
fahminurk <fahminurk31@gmail.com>
2023-04-13 23:32:38 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./redux/store";
+import { Provider } from "react-redux";
+import AuthProvider from "./hoc/AuthProvider";
+import thunk from "redux-thunk";
 
+const store = configureStore({ reducer: rootReducer, middleware: [thunk] });
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ChakraProvider>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <ChakraProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
